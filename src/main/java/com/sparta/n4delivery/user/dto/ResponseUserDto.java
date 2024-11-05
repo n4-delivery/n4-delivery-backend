ba6ebc9dc40bfecf5f2684dff39f6e9eb6289c81
MIN GI SEO <syg9111@naver.com>
2024-11-05 22:54:36 +0900

    작업 내용을 issue-15 브랜치로 옮김

--- src/main/java/com/sparta/n4delivery/user/dto/ResponseUserDto.java
+++ src/main/java/com/sparta/n4delivery/user/dto/ResponseUserDto.java
@@ -1,4 +1,20 @@
 package com.sparta.n4delivery.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResponseUserDto {
+    private String token;
+    private Long userId;
+    private String nickname; // 추가: 닉네임
+    private String email;
+
+    public ResponseUserDto(String token, Long userId, String nickname, String email) {
+        this.token = token;
+        this.userId = userId;
+        this.nickname = nickname;
+        this.email = email;
+    }
 }
--- src/main/java/com/sparta/n4delivery/user/repository/UserRepository.java
+++ src/main/java/com/sparta/n4delivery/user/repository/UserRepository.java
@@ -1,11 +1,10 @@
 package com.sparta.n4delivery.user.repository;
 
 import com.sparta.n4delivery.user.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 }
